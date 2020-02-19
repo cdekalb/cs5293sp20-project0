843ec79d464c65d6f81d7af2a5861dee907be3ee
Creighton DeKalb <cdekalb@ou.edu>
2020-02-19 21:41:55 +0000

    test_download.py is working

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,8 @@
 
 # Test to ensure that the function downloads data
 
+import urllib.request
+
 def test_download():
     url = ("http://normanpd.normanok.gov/filebrowser_download/657/2020-02-11%20Daily%20Incident%20Summary.pdf")
     data = urllib.request.urlopen(url).read()
